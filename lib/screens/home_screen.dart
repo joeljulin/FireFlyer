47b82d1600ddf71c306a22eee3a7512970dd9bc5
Joel961 <JMJ96@pitt.edu>
2022-11-14 20:28:56 -0500

    Fixed image display problem

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,31 +8,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [
-            Color.fromARGB(255, 176, 69, 39),
-            Color.fromARGB(255, 0, 0, 0)
-          ])),
-      child: Scaffold(
-          // By default, Scaffold background is white
-          // Set its value to transparent
-          backgroundColor: Colors.transparent,
-          body: CustomScrollView(
-            controller: _scrollController,
-            slivers: const [
-              SliverToBoxAdapter(
-                child: ArticleCarousel(
-                  key: PageStorageKey('editorsPicks'),
-                  title: 'Editors Picks',
-                  articleList: editorsPicks,
-                ),
-              )
-            ],
-          )
-      )
-    );
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+              Color.fromARGB(255, 176, 69, 39),
+              Color.fromARGB(255, 0, 0, 0)
+            ])),
+        child: Scaffold(
+            // By default, Scaffold background is white
+            // Set its value to transparent
+            backgroundColor: Colors.transparent,
+            body: CustomScrollView(
+              controller: _scrollController,
+              slivers: const [
+                SliverToBoxAdapter(
+                  child: ArticleCarousel(
+                    key: PageStorageKey('editorsPicks'),
+                    title: 'Editors Picks',
+                    articleList: editorsPicks,
+                  ),
+                )
+              ],
+            )));
   }
 }
