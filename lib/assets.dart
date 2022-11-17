756c6797ab1679212844ff3a8a28f706e9473c0b
Joel961 <JMJ96@pitt.edu>
2022-11-17 14:38:27 -0500

    Added variety of articles

--- lib/assets.dart
+++ lib/assets.dart
@@ -6,4 +6,9 @@ class Assets {
   static const String resume = '${_images}resume.png';
   static const String logo = '${_images}logo.png';
   static const String image4 = '${_images}image4.png';
+  static const String pelosi = '${_images}pelosi.png';
+  static const String houseRep = '${_images}house_rep.png';
+  static const String stateOfEmergency = '${_images}state_of_emergency.png';
+  static const String offDuty = '${_images}off_duty.png';
+  static const String racialDisp = '${_images}racial_disparity.png';
 }
--- lib/data/data.dart
+++ lib/data/data.dart
@@ -2,7 +2,8 @@ import 'package:FireFlyer/models/models.dart';
 import 'package:FireFlyer/assets.dart';
 
 const Content recentHeader = Content(
-  title: 'This Is Not Ukraine’s Fault’: Tensions Ease Over Missile Strike in Poland',
+  title:
+      'This Is Not Ukraine’s Fault’: Tensions Ease Over Missile Strike in Poland',
   imageUrl: Assets.image4,
   articleUrl: 'Test',
   description: 'test',
@@ -14,46 +15,52 @@ const List<Content> previews = [
 
 const List<Content> editorsPicks = [
   Content(
-      title: 'How SBF fooled everyone—including me 1',
+      title: 'How SBF fooled everyone—including me',
       imageUrl: Assets.FTX,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 2',
-      imageUrl: Assets.FTX,
+      title: 'Nancy Pelosi stands down as leader of US House Democrats',
+      imageUrl: Assets.pelosi,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 1',
-      imageUrl: Assets.FTX,
+      title:
+          'House Republicans plan investigations and possible impeachments with new majority',
+      imageUrl: Assets.houseRep,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 2',
-      imageUrl: Assets.FTX,
+      title:
+          'State of emergency declared as Buffalo, New York, braces for brunt of lake effect snow storm',
+      imageUrl: Assets.stateOfEmergency,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 1',
-      imageUrl: Assets.FTX,
+      title:
+          'Off-duty New York police officer fatally shoots a woman and wounds another before dying by suicide',
+      imageUrl: Assets.offDuty,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 2',
-      imageUrl: Assets.FTX,
+      title:
+          'Racial disparities in fertility care have persisted for years. Here’s why.',
+      imageUrl: Assets.racialDisp,
       articleUrl: 'Test',
       description: 'Test'),
 ];
 
 const List<Content> mostRead = [
   Content(
-      title: 'Men Walk On Moon',
+      title:
+          'Men Walk On Moon: Astronauts land on a plain after steering past crater',
       imageUrl: Assets.moon,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 2',
-      imageUrl: Assets.FTX,
+      title:
+          'State of emergency declared as Buffalo, New York, braces for brunt of lake effect snow storm',
+      imageUrl: Assets.stateOfEmergency,
       articleUrl: 'Test',
       description: 'Test'),
 ];
@@ -68,12 +75,13 @@ const List<Content> resumeReading = [
 
 const List<Content> trending = [
   Content(
-      title: 'How SBF fooled everyone—including me 1',
-      imageUrl: Assets.FTX,
+      title:
+          'Off-duty New York police officer fatally shoots a woman and wounds another before dying by suicide',
+      imageUrl: Assets.offDuty,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 2',
+      title: 'How SBF fooled everyone—including me',
       imageUrl: Assets.FTX,
       articleUrl: 'Test',
       description: 'Test'),
@@ -81,13 +89,14 @@ const List<Content> trending = [
 
 const List<Content> favorites = [
   Content(
-      title: 'How SBF fooled everyone—including me 1',
+      title: 'How SBF fooled everyone—including me',
       imageUrl: Assets.FTX,
       articleUrl: 'Test',
       description: 'Test'),
   Content(
-      title: 'How SBF fooled everyone—including me 2',
-      imageUrl: Assets.FTX,
+      title:
+          'State of emergency declared as Buffalo, New York, braces for brunt of lake effect snow storm',
+      imageUrl: Assets.stateOfEmergency,
       articleUrl: 'Test',
       description: 'Test'),
 ];
